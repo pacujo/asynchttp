48476179f7d8a91c798e91ac94916118bd72b381
Niko Böckerman <niko.bockerman@f-secure.com>
2020-11-24 15:12:00 +0200

    Add HttpOpJockeyPtr to ease use in C++
    HttpOpJockeyPtr is a wrapper of http_op_jockey_t* in unique_ptr with
    custom deleter which closes the jockeyed http operation.

--- include/http_op_jockey.h
+++ include/http_op_jockey.h
@@ -39,4 +39,26 @@ byte_array_t *http_op_response_release_body(http_op_response_t *response);
 
 #ifdef __cplusplus
 }
+
+#include <functional>
+#include <memory>
+
+namespace fsecure {
+namespace asynchttp {
+
+// std::unique_ptr for http_op_jockey_t with custom deleter.
+using HttpOpJockeyPtr =
+    std::unique_ptr<http_op_jockey_t, std::function<void(http_op_jockey_t *)>>;
+
+// Create HttpOpJockeyPtr that takes ownership of the provided http_op_jockey_t.
+// Pass nullptr to create an instance which doesn't contain any http_op_jockey_t
+// object.
+inline HttpOpJockeyPtr make_http_op_jockey_ptr(http_op_jockey_t *jockey)
+{
+    return { jockey, http_op_jockey_close };
+}
+
+} // namespace asynchttp
+} // namespace fsecure
+
 #endif
